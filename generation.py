d11851279cc8687397f59e2a87cc33e3df542a05
Adamb823 <adambousader@gmail.com>
2026-07-29 10:06:41 +1200

    Implement fighter generation function
    Added a function to generate a fighter with random attributes.

--- generation.py
+++ generation.py
@@ -1,1 +1,27 @@
+import random
+
+from constants import ARCHETYPES, FIRST_NAMES, LAST_NAMES, STAT_KEYS, WEIGHT_CLASSES
+from models import Fighter
+
+
+def generate_fighter():
+    first_name = random.choice(FIRST_NAMES)
+    last_name = random.choice(LAST_NAMES)
+
+    full_name = first_name + " " + last_name
+
+    fighter_stats = {}
+
+    for stat in STAT_KEYS:
+        fighter_stats[stat] = random.randint(35, 75)
+
+    fighter_age = random.randint(18, 36)
+    fighter_weight = random.choice(WEIGHT_CLASSES)
+    fighter_style = random.choice(ARCHETYPES)
+
+    new_fighter = Fighter(
+        full_name, fighter_age, fighter_weight, fighter_style, fighter_stats
+    )
+
+    return new_fighter
 
