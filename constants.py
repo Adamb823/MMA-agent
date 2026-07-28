678a3baca921125942e75128b4221960bc319a2d
Adamb823 <adambousader@gmail.com>
2026-07-29 10:05:51 +1200

    Add constants for weight classes and fighter attributes
    Define constants for weight classes, archetypes, stats, and names.

---
+++ constants.py
@@ -0,0 +1,60 @@
+WEIGHT_CLASSES = [
+    "Flyweight",
+    "Bantamweight",
+    "Featherweight",
+    "Lightweight",
+    "Welterweight",
+    "Middleweight",
+    "Light Heavyweight",
+    "Heavyweight",
+]
+
+ARCHETYPES = ["Striker", "Wrestler", "Grappler", "All-Rounder"]
+
+STAT_KEYS = ["striking", "grappling", "wrestling", "cardio", "power", "chin"]
+
+FIRST_NAMES = [
+    "Jake",
+    "Miguel",
+    "Chris",
+    "Andre",
+    "Kai",
+    "Dmitri",
+    "Ryo",
+    "Marcus",
+    "Liam",
+    "Tane",
+    "Diego",
+    "Sione",
+    "Noah",
+    "Hamish",
+    "Tyrell",
+    "Ezekiel",
+    "Ravi",
+    "Cole",
+    "Bruno",
+    "Ash",
+]
+
+LAST_NAMES = [
+    "Reyes",
+    "Walker",
+    "Novak",
+    "Faamausili",
+    "Tui",
+    "Petrov",
+    "Nakamura",
+    "Steele",
+    "Grant",
+    "Vaka",
+    "Delgado",
+    "Iona",
+    "Brennan",
+    "Okafor",
+    "Hardy",
+    "Silva",
+    "Marsh",
+    "Kowalski",
+    "Reid",
+    "Ngata",
+]
